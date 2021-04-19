d7a7ffe6e4f56aa61b82054ddc91112367b234e8
ace kloun <alexey.rushev@gmail.com>
2021-04-19 12:47:27 +0300

    Добавление проекта

---
+++ ExamenVasnaPM03/ExamenVasnaPM03/Pogoda.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamenVasnaPM03
+{
+    class Pogoda
+    {
+        int vlagnost;
+        int temperatura;
+        int davlenie;
+
+        public int Vlagnost {
+            get { return this.vlagnost; }
+            set { this.vlagnost = value; }
+        }
+
+        public int Temperatura
+        {
+            get { return this.temperatura; }
+            set { this.temperatura = value; }
+        }
+
+        public int Davlenie
+        {
+            get { return this.davlenie; }
+            set { this.davlenie = value; }
+        }
+    }
+}
